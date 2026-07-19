d0c753437f973c191f3f619c456a9e70146b3cf7
COG-tech <chadamariigrant@gmail.com>
2026-07-20 03:20:36 +0800

    Refresh offline cache for backdrop skin

--- sw.js
+++ sw.js
@@ -1,12 +1,15 @@
-const CACHE_NAME = 'toxic-teddies-arrow-escape-v1';
+const CACHE_NAME = 'toxic-teddies-arrow-escape-v2';
 const CORE_ASSETS = [
   './',
   './index.html',
   './styles.css',
   './arrow-extras.css',
+  './skin.css',
   './app.js',
+  './skin.js',
   './characters.js',
   './manifest.webmanifest',
+  './assets/backdrops/toxic-toby-expression-sheet.svg',
   './.nojekyll'
 ];
 
