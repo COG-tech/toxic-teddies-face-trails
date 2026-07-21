5406b013793fd66afc107fc8b156a3896f8f9313
COG-tech <chadamariigrant@gmail.com>
2026-07-21 23:10:22 +0800

    Use native-safe Capacitor app name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.cogtech.toxicteddies',
-  appName: 'Toxic Teddies: Arrow Escape',
+  appName: 'Toxic Teddies',
   webDir: 'dist',
   server: {
     androidScheme: 'https',
